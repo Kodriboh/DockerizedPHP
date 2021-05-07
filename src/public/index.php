be31d2081987c84c7fca1bf56b82dc4759649322
LukeMcCann <developer-lukemccann@outlook.com>
2021-05-07 23:42:32 +0100

    Fix autoload issue

--- src/public/index.php
+++ src/public/index.php
@@ -1,7 +1,11 @@
-<?php declare(strict_types = 1); 
+<?php declare(strict_types = 1);
+
+use App\config\DatabaseConnector;
 
 define('ROOT_DIR', dirname(__DIR__));
 
 require_once ROOT_DIR . '/bootstrap/app.php';
 
-echo $_ENV['OKTAAUDIENCE'];
+$dbConnection = (new DatabaseConnector())->getConnection();
+
+var_dump($dbConnection);
